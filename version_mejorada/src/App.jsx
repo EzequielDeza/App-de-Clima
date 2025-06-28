2f4d516a908fe30e32abafd35dfcba78970e52f9
Ezequiel Deza <401006@sistemas.frc.utn.edu.ar>
2025-06-28 17:24:32 -0300

    estructura de React creada y funcionando

---
+++ version_mejorada/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import FormularioBusqueda from './components/FormularioBusqueda'
+
+
+function App() {
+
+  return (
+    <>
+      <FormularioBusqueda />
+    </>
+  )
+}
+
+export default App
