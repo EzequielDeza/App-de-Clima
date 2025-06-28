f774dbfb45176736f23fdb90e5699b29c51105a2
Ezequiel Deza <401006@sistemas.frc.utn.edu.ar>
2025-06-28 20:55:41 -0300

    Formulario y lista de nombre de ciudades terminado (Form. que filtra por nombre de ciudad)

--- version_mejorada/src/App.jsx
+++ version_mejorada/src/App.jsx
@@ -1,14 +1,67 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import FormularioBusqueda from './components/FormularioBusqueda'
-
+import { buscarCiudades } from './services/ciudad.service'
+import { useForm } from 'react-hook-form'
 
 function App() {
 
+  const [ciudad, setCiudad] = useState()
+
+  {/* Comento el useEffect porque sino me busca todas las ciudades al iniciar la App y eso me consume la prueba gratuita de la API */}
+  { /*
+  useEffect(() => {
+    cargarCiudades()
+  }, [])
+  */ }
+
+  const cargarCiudades = async (ciudad ) => {
+    try {
+      const data = await buscarCiudades(ciudad)
+      setCiudad(data)
+      {/* console.log(data) */}
+    } catch (error) {
+      console.error('Error al cargar las ciudades:', error)
+    }
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm()
+
+  const onSubmit = (data) => {
+    cargarCiudades(data.ciudad)
+  }
   
   return (
     <>
-      <FormularioBusqueda />
+    <section>
+      <form className="city_name_form" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="ciudad">Nombre Ciudad:</label>
+        <input 
+          type="text" 
+          id="ciudad" 
+          {...register("ciudad", {required: true},)}
+        />
+        {errors.ciudad && <span>Este campo es obligatorio</span>}
+        <button type="submit">Buscar</button>
+      </form>
+    </section>
+
+    <section className="city_list">
+      <ul>
+        {ciudad && ciudad.map((c) => (
+          <li key={c.id}>
+            <h2>{c.name}</h2>
+            <p>País: {c.country}</p>
+            <p>Latitud: {c.latitude}</p>
+            <p>Longitud: {c.longitude}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
     </>
   )
 }
--- version_mejorada/src/components/FormularioBusqueda.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FormularioBusqueda() {
-    return (
-        <div>FormularioBusqueda</div>
-    )
-}
